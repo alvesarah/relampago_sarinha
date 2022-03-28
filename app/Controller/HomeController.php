3caf4ff7aa29631469294e5f59a955b80a2890c4
alvesarah <sarah.alves8@hotmail.com>
2022-03-28 20:32:01 -0300

    Passando informações da tabela do controller para o home.html

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -4,7 +4,17 @@ class HomeController{
     public function index(){
         try{
             $colecaoVeiculos = Veiculos::selecionarTodos();
-            var_dump($colecaoVeiculos);
+            
+            $loader = new \Twig\Loader\FilesystemLoader('app/View');
+			$twig = new \Twig\Environment($loader);
+            $template = $twig->load('home.html');
+
+            $parametros = array();
+            $parametros['veiculos'] = $colecaoVeiculos;
+
+            $conteudo = $template->render($parametros);
+            echo $conteudo;
+
         } catch(Exception $e){
             // Imprimir mensagem de erro se não houver registro
             echo $e->getMessage();
